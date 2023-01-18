d5ec3aa82137ccdb05bd0bf78fe14301b67cff61
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2023-01-18 21:44:35 +0800

    add Task03

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -8,7 +8,7 @@ using static System.Console;
 Clear();
 Write("Введите размер массива: ");
 string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-int[] array = GetArray(int.Parse(parameters[0]), 1, 10);
+int[] array = GetArray(int.Parse(parameters[0]), 1, 100);
 PrintArray(array);
 WriteLine();
 Write($"Сумма элементов на нечетных позициях: {GetSumOddIndex(array)}");
--- Task03/Program.cs
+++ Task03/Program.cs
@@ -4,3 +4,55 @@
 максимальным и минимальным элементов массива.
 [3,3 7,1 22,5 2,2 78,2] -> 76
 */
+using static System.Console;
+Clear();
+Write("Введите размер массива: ");
+string[] parameters = ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] array = GetArray(int.Parse(parameters[0]), 1, 100);
+PrintArray(array);
+WriteLine();
+Write($"Разница между max и min элементами: {GetDiff(array)}");
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] resultArray = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        resultArray[i] = rnd.Next(minValue, maxValue + 1);
+    }
+    return resultArray;
+}
+int GetDiff(int[] inArray)
+{
+    int max = inArray[0];
+    int min = inArray[0];
+    int diff = 0;
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (max < inArray[i])
+        {
+            max = inArray[i];
+        }
+        if (min > inArray[i])
+        {
+            min = inArray[i];
+        }
+        diff = max - min;
+    }
+    return diff;
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("Заданный массив: [");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]},");
+    }
+    Write($"{inArray[inArray.Length - 1]}]");
+}
+
+
